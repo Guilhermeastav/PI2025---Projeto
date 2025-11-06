6eace63375178e9dad7b480279019152e61a1373
Guilhermeastav <tavaresguilhermetb@gmail.com>
2025-11-06 20:02:34 -0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace PI2025___Projeto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHistorico = new System.Windows.Forms.Button();
+            this.btnAluguel = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnDeslogar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnHistorico
+            // 
+            this.btnHistorico.BackColor = System.Drawing.Color.Black;
+            this.btnHistorico.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold);
+            this.btnHistorico.ForeColor = System.Drawing.Color.LightBlue;
+            this.btnHistorico.Location = new System.Drawing.Point(545, 260);
+            this.btnHistorico.Name = "btnHistorico";
+            this.btnHistorico.Size = new System.Drawing.Size(165, 42);
+            this.btnHistorico.TabIndex = 0;
+            this.btnHistorico.Text = "HISTÓRICO";
+            this.btnHistorico.UseVisualStyleBackColor = false;
+            this.btnHistorico.Click += new System.EventHandler(this.btnHistorico_Click);
+            // 
+            // btnAluguel
+            // 
+            this.btnAluguel.BackColor = System.Drawing.Color.Black;
+            this.btnAluguel.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAluguel.ForeColor = System.Drawing.Color.LightBlue;
+            this.btnAluguel.Location = new System.Drawing.Point(97, 258);
+            this.btnAluguel.Name = "btnAluguel";
+            this.btnAluguel.Size = new System.Drawing.Size(165, 44);
+            this.btnAluguel.TabIndex = 1;
+            this.btnAluguel.Text = "ALUGUEL";
+            this.btnAluguel.UseVisualStyleBackColor = false;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.Black;
+            this.btnLogin.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.LightBlue;
+            this.btnLogin.Location = new System.Drawing.Point(318, 277);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(165, 25);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "LOGIN";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.LightBlue;
+            this.textBox1.Location = new System.Drawing.Point(239, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(327, 33);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "TESTE ALUGUEL CARROS V1";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnDeslogar
+            // 
+            this.btnDeslogar.BackColor = System.Drawing.Color.LightGray;
+            this.btnDeslogar.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDeslogar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnDeslogar.Location = new System.Drawing.Point(545, 415);
+            this.btnDeslogar.Name = "btnDeslogar";
+            this.btnDeslogar.Size = new System.Drawing.Size(101, 23);
+            this.btnDeslogar.TabIndex = 4;
+            this.btnDeslogar.Text = "DESLOGAR";
+            this.btnDeslogar.UseVisualStyleBackColor = false;
+            this.btnDeslogar.Click += new System.EventHandler(this.btnDeslogar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = global::PI2025___Projeto.Properties.Resources.unnamed;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDeslogar);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.btnAluguel);
+            this.Controls.Add(this.btnHistorico);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHistorico;
+        private System.Windows.Forms.Button btnAluguel;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnDeslogar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PI2025___Projeto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHistorico_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDeslogar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
